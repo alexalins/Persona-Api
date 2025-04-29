8e46d4edcfd5dbde716fabe61352a0df69428908
Alexa Gonçalves Lins <alexa.glinsoo@gmail.com>
2025-04-29 19:45:04 -0300

    instanando gorilla mux

--- routes/routes.go
+++ routes/routes.go
@@ -4,10 +4,13 @@ import (
 	"api-persona/controllers"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/persona", controllers.GetAllPersonas)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/persona", controllers.GetAllPersonas)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
